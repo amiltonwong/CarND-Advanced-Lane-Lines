c850b00b074aa6580b52f16e5ce3becc325f866c
Milton Wong <amiltonwong@gmail.com>
2017-04-02 12:50:30 +0800

    commit

--- bin_thresh.py
+++ bin_thresh.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def thresholded_binary(undistorted_img):
     """
     :param undistorted_img:
-        Source image for thresholding that has already been undistorted.
+        Source image (undistorted) for thresholding (combination thresholding from Sobel x gradient + HLS S channel + HSV V channel)
     
     :return:
         Returns a colour binary image for visualisation purposes and a
@@ -30,20 +30,40 @@ def thresholded_binary(undistorted_img):
     # Note: img is the undistorted image
     hls = cv2.cvtColor(undistorted_img, cv2.COLOR_RGB2HLS).astype("float")
     s_channel = hls[:,:,2]
-        
-    # Threshold color channel
-    s_thresh_min = 170
-    s_thresh_max = 255
     s_binary = np.zeros_like(s_channel)
-    s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+    s_thresh_min = 100
+    s_thresh_max = 255
+    s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1    
+
+    # Convert to HSV color space and separate the V channel
+    # Note: img is the undistorted image
+    hsv = cv2.cvtColor(undistorted_img, cv2.COLOR_RGB2HSV).astype("float")
+    v_channel = hsv[:,:,2]
+    v_binary = np.zeros_like(v_channel)
+    v_thresh_min = 50
+    v_thresh_max = 255
+    v_binary[(v_channel >= v_thresh_min) & (v_channel <= v_thresh_max)] = 1  
     
-    # Stack each channel to view their individual contributions in green (sxbinary) and blue (s_binary) respectively
+    # use bitand to obtian the threshold binary for s channel and v channeel
+    s_v_binary = np.zeros_like(s_channel)
+    s_v_binary[(s_binary == 1) & (v_binary == 1)] =  1
+    
+    # Stack each channel to view their individual contributions in green (sxbinary) and blue (s_v_binary) respectively
     # This returns a stack of the two binary images, whose components you can see as different colors
-    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary))
+    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_v_binary))
     
     # Combine the two binary thresholds
     combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+    combined_binary[(s_v_binary == 1) | (sxbinary == 1)] = 1
+
+    # create a mask to filter out pixels inside the shape from src points
+    vertices = np.array([[[575+50, 464+50],
+                 [730-50, 464+50],
+                 [1100-100, 682],
+                 [280+100, 682]]], dtype=np.int32 )
+    mask = np.ones_like(combined_binary)
+    cv2.fillPoly(mask, vertices, 0)
+    combined_binary = cv2.bitwise_and(combined_binary, mask)
 
     return color_binary, combined_binary
 
